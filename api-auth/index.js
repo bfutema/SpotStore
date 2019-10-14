8b8c1e0252c95d2b50d641d19ff803d50aa71884
Bruno Amaral Futema <bruno.futema@outlook.com>
2019-10-14 00:53:03 -0300

    feature: created routes and base project

---
+++ api-auth/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/api', require('./src/routes'));
+
+app.listen(9000);
